45e2ed8a9749553038cc586085a26320c9edb920
Kenton <kenton@localhost.localdomain>
2019-02-01 21:26:04 -0500

    added react code

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -5,6 +5,7 @@ import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_debugtoolbar import DebugToolbarExtension
 
 
 # instantiate the db
@@ -15,6 +16,7 @@ def create_app(script_info=None):
 
     # instantiate the app
     app = Flask(__name__)
+    toolbar = DebugToolbarExtension()
 
     # set config
     app_settings = os.getenv('APP_SETTINGS')
@@ -22,6 +24,7 @@ def create_app(script_info=None):
 
     # set up extensions
     db.init_app(app)
+    toolbar.init_app(app)
 
     # register blueprints
     from project.api.users import users_blueprint
